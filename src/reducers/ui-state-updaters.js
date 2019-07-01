e87ee829d3387f10717ca71a42a37ffb2a4e8f44
Giuseppe Macri <gmacri@uber.com>
2019-07-01 13:00:54 -0700

    Able to select polygon
    Signed-off-by: Giuseppe Macri <gmacri@uber.com>

--- src/reducers/ui-state-updaters.js
+++ src/reducers/ui-state-updaters.js
@@ -171,7 +171,8 @@ export const DEFAULT_EXPORT_MAP = {
 };
 
 export const DEFAULT_EDITOR = {
-  mode: EDITOR_MODES.READ_ONLY
+  mode: EDITOR_MODES.READ_ONLY,
+  selectedFeature: null
 };
 
 /**
@@ -575,5 +576,13 @@ export const setEditorModeUpdater = (state, {payload: mode}) => ({
   editor: {
     ...state.editor,
     mode
+  }
+});
+
+export const setSelectedFeatureUpdater = (state, {payload: selectedFeature}) => ({
+  ...state,
+  editor: {
+    ...state.editor,
+    selectedFeature
   }
 });
